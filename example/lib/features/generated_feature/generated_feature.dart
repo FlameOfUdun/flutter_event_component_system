a2809fe4c73572897d1d604e5245c59cd504e9c8
Ehsan Rashidi <dr.e.rashidi@gmail.com>
2026-04-09 01:36:01 +0100

    feat: update example to new ECS DSL

--- example/lib/features/generated_feature/generated_feature.dart
+++ example/lib/features/generated_feature/generated_feature.dart
@@ -1,48 +1,21 @@
 import 'package:flutter_event_component_system/flutter_event_component_system.dart';
 import 'package:flutter_event_component_system_annotations/flutter_event_component_system_annotations.dart';
 
-part 'generated_feature.ecs.dart';
+part 'generated_feature.ecs.g.dart';
 
-@ECSComponentDefinition()
-const int health = 0;
+@Component() int health = 0;
 
-@ECSEventDefinition(
-  description: 'Resets health to 0 for testing purposes.',
-)
-const Object resetHealth = Object();
+@Event(description: 'Resets health to 0 for testing purposes.')
+void resetHealth() { applyResetHealth(); }
 
-@ECSDataEventDefinition()
-const int addHealth = 10;
+@Event() void addHealth(int amount) { applyAddHealth(amount); }
 
-@ECSReactiveSystemDefinition(reactsTo: {addHealth}, interactsWith: {health}, reactsIf: addHealthIf)
-void applyAddHealth(ECSSystemReference system) {
-  final component = system.getComponent(health);
-  final event = system.getDataEvent(addHealth);
-  component.value += event.data;
+@ReactiveSystem()
+void applyAddHealth(int amount) {
+  health += amount;
 }
 
-bool addHealthIf(ECSSystemReference system) {
-  final component = system.getComponent(health);
-  final event = system.getDataEvent(addHealth);
-  return component.value + event.data <= 100;
-}
-
-@ECSReactiveSystemDefinition(reactsTo: {resetHealth}, interactsWith: {health})
-void applyResetHealth(ECSSystemReference system) {
-  final component = system.getComponent(health);
-  component.update(0, force: true);
-}
-
-@ECSFeatureDefinition()
-void buildGeneratedFeature(
-  ECSFeatureReference feature, {
-  bool mock = false,
-}) {
-  if (mock) {
-    feature.addEvent(resetHealth);
-  }
-  feature.addComponent(health);
-  feature.addDataEvent(addHealth);
-  feature.addReactiveSystem(applyAddHealth);
-  feature.addReactiveSystem(applyResetHealth);
+@ReactiveSystem()
+void applyResetHealth() {
+  health = 0;
 }
